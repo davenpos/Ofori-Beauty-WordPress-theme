df90b7c0af7cf0822e77948f49d6a2c771302f7e
davenpos <davenpos@uwindsor.ca>
2023-11-21 21:37:54 -0500

    Removed irrelevant categories PHP code from single-product.php page

--- single-product.php
+++ single-product.php
@@ -1,15 +1,7 @@
 <?php get_header(); ?>
 <div class="blogColumns">
     <div class="textContent blogPage">
-        <?php while (have_posts()): the_post();
-            $categories = get_the_category();
-            $categoryList = '';
-
-            if ($categories):
-                foreach ($categories as $cat):
-                    $categoryList .= '<a href="' . get_category_link($cat->term_id) .'">' . $cat->cat_name . '</a>, ';
-                endforeach;
-            endif; ?>
+        <?php while (have_posts()): the_post(); ?>
             <h1><?php the_title(); ?></h1>
             <p><?php the_content(); ?></p>
         <?php endwhile; ?>
