a7df861fba2b504b407f78c66e86961b52a7cfcc
davenpos <davenpos@uwindsor.ca>
2023-12-06 19:56:05 -0500

    Made more style changes and added functionality for color customizer

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -6,6 +6,8 @@ if (empty($product) || !$product->is_visible()) {
 	return;
 }
 ?>
-<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-<div class="serviceDescription"><?php the_excerpt(); ?></div>
-<p class="price"><?php echo $product->get_price_html(); ?></p>
+<li>
+	<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+	<div class="serviceDescription"><?php the_excerpt(); ?></div>
+	<p class="price"><?php echo $product->get_price_html(); ?></p>
+</li>
